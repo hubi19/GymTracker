77aebe261609d409d56ce1907cbf9cf537884b1c
Hubert Solecki <hsolecki1@gmail.com>
2025-04-12 18:58:58 +0200

    feat: pwa

--- gym-tracker/next.config.ts
+++ gym-tracker/next.config.ts
@@ -1,7 +1,19 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
+import withPWA from 'next-pwa';
+import path from 'path';
+
+const isDev = process.env.NODE_ENV === 'development';
 
 const nextConfig: NextConfig = {
   output: 'export',
+  images: {
+    unoptimized: true, // wymagane przy 'output: export'
+  },
 };
 
-export default nextConfig;
+export default withPWA({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: isDev, // wyłącz PWA w trybie dev
+})(nextConfig);
